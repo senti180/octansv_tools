b7230bc777286e8047f618782f5c13a0c0cf083a
Bence Csizmadia <47395613+senti180@users.noreply.github.com>
2021-12-20 09:14:37 +0100

    Delete live directory

--- live/js/copy.js
+++
@@ -1,9 +0,0 @@
-function CopyToClipboard(element)
-{
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-    alert("Másolva!");
-}
